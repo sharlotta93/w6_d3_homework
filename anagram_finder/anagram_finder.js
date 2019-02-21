f8a2fe9e8b946fe36699dce027eac386de0a917a
Abigaila Ekierte <arekiert@gmail.com>
2019-02-21 08:42:55 +0000

    refactoring anagram

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -5,23 +5,23 @@ const AnagramFinder = function (word) {
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
   function regularize (string){ return string.toLowerCase().split('').sort().join('')};
   let anagramArray = []
-  let mainWord = regularize(this.word)
 
   for (var word of otherWords) {
     if (this.word === word) break;
-    var newWord = regularize(word);
-    if (newWord === mainWord) {
+    if (regularize(word) === regularize(this.word)) {
       anagramArray.push(word);
     }
   }
   return anagramArray;
 }
 
-module.exports = AnagramFinder;
-
 // AnagramFinder.prototype.findAnagrams = function (otherWords) {
+//   function regularize (string){ return string.toLowerCase().split('').sort().join('')};
+//
 //   let anagramArray = otherWords.filter( (word) => {
-//     (this.word.split('').sort().join('') === word.split('').sort().join(''))
+//     (regularize(this.word) === regularize(word))
 //   });
 //   return anagramArray;
 // }
+
+module.exports = AnagramFinder;
