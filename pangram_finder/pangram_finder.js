fafd624e2cbed9f449060f2ca58e0623df720718
Abigaila Ekierte <arekiert@gmail.com>
2019-02-21 08:58:59 +0000

    updating pangram

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -4,10 +4,13 @@ const PangramFinder = function (phrase) {
 }
 
 PangramFinder.prototype.isPangram = function () { //regular expressions
-  let phrase = this.phrase.toLowerCase().replace(/[^a-z]/g,'') //at the start replace anything with a combination with zero to many is a-z
+  let phrase = this.phrase.toLowerCase().replace(/[^a-z]/g,'') //at the start replace any symbol with space
       for (var i = 0; i < this.alphabet.length; i++) {
-          return (phrase.indexOf(this.alphabet[i]) < 0) ? false : true;
+          return (phrase.indexOf(this.alphabet[i]) < 0) ? false : true; //The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
     }
 }
+
+
+// I can also remove all duplicate letter and check whether it's still 26
 
 module.exports = PangramFinder;
